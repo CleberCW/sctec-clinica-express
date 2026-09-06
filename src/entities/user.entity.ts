ab29d8b6e8775147a4d881ed9979cee6b54b3d11
Cleber <cleberws.leal@gmail.com>
2026-09-06 13:03:25 -0300

    feat: create entities

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,13 +1,41 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  JoinTable,
+  ManyToMany,
+  CreateDateColumn,
+} from 'typeorm';
+import { Role } from './role.entity.ts';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @Column({ type: 'varchar' })
+  @Column({ type: 'varchar', nullable: false })
   firstName!: string;
 
-  @Column({ type: 'varchar' })
+  @Column({ type: 'varchar', nullable: false })
   lastName!: string;
+
+  @Column({ type: 'varchar', unique: true })
+  email!: string;
+
+  @Column({ type: 'varchar' })
+  hashedPassword!: string;
+
+  @ManyToMany(() => Role, (role) => role.users, {
+    cascade: true,
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinTable()
+  roles!: Role;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  createdAt!: Date;
 }
