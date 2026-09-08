04cdf32f97257315c81b0119bc850389ce7861a0
Cleber <cleberws.leal@gmail.com>
2026-09-08 08:26:25 -0300

    fix: migration not reverting roles seeding

--- sql/migrations/1788826691571-SeedRoles.ts
+++ sql/migrations/1788826691571-SeedRoles.ts
@@ -13,6 +13,14 @@ export class SeedRoles1788826691571 implements MigrationInterface {
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
+    DELETE FROM "user_roles_role"
+    WHERE "roleId" IN (
+      SELECT "id"
+      FROM "role"
+      WHERE "name" IN ('admin', 'user', 'owner')
+    );
+  `);
+    await queryRunner.query(`
       DELETE FROM "role"
       WHERE "name" IN ('admin', 'user', 'owner')
     `);
